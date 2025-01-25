15a2a878391df3a0e6004fbbc24f022144825f2e
Stephen Dorssers <sdorssers7@gmail.com>
2025-01-25 15:54:15 -0500

    exercise 3.4

--- PhonebookBackend/index.js
+++ PhonebookBackend/index.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const app = express()
 
+const persons = [
+  { 
+    "id": "1",
+    "name": "Donnie Darko", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
 app.get('/api/persons',(request,response) => {
     response.json(persons)
 })
@@ -14,6 +37,12 @@ app.get(`/api/persons/:id`,(request,response) => {
   } else {
     response.status(404).end()
   }
+}) 
+
+app.delete('/api/persons/:id',(request,response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
 })
 
 app.get('/info',(request,response) => {
@@ -27,25 +56,3 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-const persons = [
-    { 
-      "id": "1",
-      "name": "Donnie Darko", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
