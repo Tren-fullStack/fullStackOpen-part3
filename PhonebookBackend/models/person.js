1be13002e7de9c54cacee436859c66cbdedc8820
Stephen Dorssers <sdorssers7@gmail.com>
2025-02-01 16:27:48 -0500

    exercise 3.21 - added custom validation messages

--- PhonebookBackend/models/person.js
+++ PhonebookBackend/models/person.js
@@ -16,24 +16,26 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
-        minLength: 3,
+        minLength: [3, `must be at least 3 letters long`],
         required: true,
         validate: {
             validator: function(valName) {
-                return /^[a-zA-Z]+$/.test(valName)
-            } 
-        },
+                return /^[a-zA-Z]+[a-z(\s)A-Z]*[a-zA-Z]+$/.test(valName)
+            },
+            message: `must only contain letters and spaces and end in a letter` 
+        }, 
     },
     number: {
         type: String,
-        minLength: 8,
+        minLength: [8, `must include at least 7 numbers`],
         required: true,
         validate: {
             validator: function(valNum) {
                 return /^\d{2,3}[-]\d+$/.test(valNum)
-            }
+            },
+            message: `format must be: 000-00000... or 00-00000...` 
         },
-    }
+    },
 })
 
 personSchema.set('toJSON', {
